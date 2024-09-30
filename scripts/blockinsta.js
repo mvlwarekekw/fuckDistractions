233a67060c999cb837f0458aceb2fafc15824ce6
Friendly Ware <friendlywaresmp@gmail.com>
2024-09-30 21:09:20 +0200

    feat: add instagram blocker

---
+++ scripts/blockinsta.js
@@ -0,0 +1,9 @@
+const css_insta = `
+main {
+    display: none !important;
+}
+`;
+
+const style_insta = document.createElement('style');
+style_insta.textContent = css_insta;
+document.head.appendChild(style_insta)
