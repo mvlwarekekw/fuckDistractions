75ecdf0c9fe4071488f21510f6c8020b63b57437
Friendly Ware <friendlywaresmp@gmail.com>
2024-09-30 21:06:02 +0200

    update: rename variables for easier future reference

--- scripts/blockyt.js
+++ scripts/blockyt.js
@@ -1,9 +1,9 @@
-const css = `
+const css_yt = `
 #shorts-container {
     display: none !important;
 }
 `;
 
-const style = document.createElement('style');
-style.textContent = css;
-document.head.appendChild(style)
+const style_yt = document.createElement('style');
+style_yt.textContent = css_yt;
+document.head.appendChild(style_yt)
