72fc4882db48ac8b41e31560f969ec03060372ad
Friendly Ware <friendlywaresmp@gmail.com>
2024-09-30 19:58:47 +0200

    feat: add youtube short blocking

---
+++ scripts/blockyt.js
@@ -0,0 +1,9 @@
+const css = `
+#shorts-container {
+    display: none !important;
+}
+`;
+
+const style = document.createElement('style');
+style.textContent = css;
+document.head.appendChild(style)
