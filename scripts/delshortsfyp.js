174d2382a49176b94cfdbbdef5be5efe844670a1
Friendly Ware <friendlywaresmp@gmail.com>
2024-10-01 21:28:31 +0200

    feat: add delete shorts on the fyp

---
+++ scripts/delshortsfyp.js
@@ -0,0 +1,14 @@
+const yt_homepage_block = () => {
+    const shelves = document.getElementsByClassName("ytd-rich-shelf-renderer")
+    if (shelves) {
+        console.log(shelves)
+        console.log(shelves.length)
+        for (let i = 0, len = shelves.length, text = ""; i < len; i++) {
+            if (shelves.item(i).id === "dismissible") {
+                shelves.item(i).innerHTML = ""
+            }
+        }
+    }
+}
+
+yt_homepage_block()
